f397ed19ccbd67e1ca06c0376fce22f84239e04f
Trandafir <andrei.florin.trandafir@hp.com>
2022-06-29 23:50:58 +0300

    added buttons iteration

--- car-app/Js files/main.js
+++ car-app/Js files/main.js
@@ -1,6 +1,10 @@
 // Global Variables
-
 let title = document.querySelector(".dynamic-p");
+let menu = document.querySelector(".menu");
+let secondMenu = document.querySelector(".second-menu");
+let btn = document.querySelectorAll(".btn");
+secondMenu.style.display = "none";
+
 
 // Video looping
 
@@ -13,3 +17,14 @@ videoAnimation.style.animationPlayState = "running";
 
 let lastname = localStorage.getItem("resulted");
 title.innerHTML = `Results for <span class="lastname">${lastname}</span>:`;
+
+
+
+// Buttons iteration
+
+function btns(item) {
+item.forEach( (b) => {b.addEventListener("click", () => {menu.style.display = "none";
+secondMenu.style.display = "block";})})
+}
+
+btns(btn);
